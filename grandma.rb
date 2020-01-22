2dfe620027514b3d49f394782cc901ec2ff9cfb3
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 03:26:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,7 @@ def speak_to_grandma (phrase="I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase
     return "HUH?! SPEAK UP, SONNY!"
+  else "NO, NOT SINCE 1938!"
+    
   end
 end
