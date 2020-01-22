d51bdf7e1145d35fc86e06d46b3e771a28f2d930
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 03:03:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -18,6 +18,6 @@ def speak_to_grandma (input)
   elsif input
     puts "HUH?! SPEAK UP, SONNY!"
   else input.uppercase?
-    puts "NO, NOT SINCE 1938!"    
+    puts "NO, NOT SINCE 1938!"
   end
 end
